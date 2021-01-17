d92ba0621ce685327046c24de35862d657f16242
Finn Bunta <fbunta@belvederetrading.com>
2021-01-17 11:47:54 -0600

    allocate score to heap 2

--- src/game.cpp
+++ src/game.cpp
@@ -64,7 +64,7 @@ void Game::Run(Controller const &controller, Renderer &renderer,
 
     // After every second, update the window title.
     if (frame_end - title_timestamp >= 1000) {
-      renderer.UpdateWindowTitle(*score, frame_count);
+      renderer.UpdateWindowTitle(score, frame_count);
       frame_count = 0;
       title_timestamp = frame_end;
     }
--- src/game.h
+++ src/game.h
@@ -11,6 +11,7 @@
 class Game {
  public:
   Game(std::size_t grid_width, std::size_t grid_height);
+  ~Game();
   void Run(Controller const &controller, Renderer &renderer,
            std::size_t target_frame_duration);
   int GetScore() const;
